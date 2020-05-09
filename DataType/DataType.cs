1df63ed87e9500e479dd35d79080e456cfd132e9
nguyenlinh3836 <54592412+nguyenlinh3836@users.noreply.github.com>
2020-05-09 14:51:28 +0700

    Add project files.

---
+++ DataType/DataType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DataType
+{
+    class DataType
+    {
+        static void Main(string[] args)
+        {
+            int intVal;
+            double dblVal;
+            String strVal;
+            intVal = 10;
+            dblVal = 3.142;
+            strVal = "Fpt Aptech";
+            Console.WriteLine("{0} is an integer vlaue", intVal);
+            Console.WriteLine("{0} is an double vlaue", dblVal);
+            Console.WriteLine("{0} is an string", strVal);
+            Console.ReadLine();
+        }
+    }
+}
